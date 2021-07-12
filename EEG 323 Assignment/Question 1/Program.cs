743db6c9c5190eb883831cfe095395d64e52d964
Ayokunle Ajala <ayokunz@gmail.com>
2021-07-12 21:36:08 +0100

    b

---
+++ EEG 323 Assignment/Question 1/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+NAME: AJALA AYOKUNLE MORALAYO
+MATRIC NUMBER: EES/18/19/0095
+DEPARTMENT: ELECTRICAL/ ELECTRONICS ENGINEERING
+COURSE: EEG 323
+**/
+using System;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you a Full-time or a Part-time student?");
+            string student = Console.ReadLine().ToLower();
+            if (student == "full-time" || student == "full")
+            {
+                Console.WriteLine("What is your course of study?");
+                string course = Console.ReadLine().ToUpper();
+                Console.WriteLine(course + " is a good course");
+            }
+            else
+            {
+                if (student == "part-time" || student == "part")
+                {
+                    Console.WriteLine("How many credit are you taking?");
+                    Console.Write("Input the number of credit: ");
+                    int credit = Convert.ToInt32(Console.ReadLine());
+                    if (credit > 6)
+                    {
+                        Console.WriteLine("Too much units for a part-time student");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proceed");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                }
+            }
+
+        }
+    }
+}
---
+++ EEG 323 Assignment/Question 2/Program.cs
@@ -0,0 +1,48 @@
+﻿/**
+NAME: AJALA AYOKUNLE MORALAYO
+MATRIC NUMBER: EES/18/19/0095
+DEPARTMENT: ELECTRICAL/ ELECTRONICS ENGINEERING
+COURSE: EEG 323
+**/
+using System;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /** this is a program for solving a simultanous equation using cramers rule
+            the form of the equation looks like :
+            ax + by = e
+            cx + dy = f 
+            **/
+            Console.WriteLine("To solve the equation, input your values");
+            Console.Write("a: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c: ");
+            double c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("d: ");
+            double d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("e: ");
+            double e = Convert.ToInt32(Console.ReadLine());
+            Console.Write("f: ");
+            double f = Convert.ToInt32(Console.ReadLine());
+            double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
+            double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
+            if ((a * d) - (b * c) == 0)
+            {
+                Console.WriteLine("The equation has no solution");
+            }
+            else
+            {
+                Console.WriteLine($"x is equal to {x}");
+                Console.WriteLine($"y is equal to {y}");
+            }
+
+
+        }
+    }
+}
